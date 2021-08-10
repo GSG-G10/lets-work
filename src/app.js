fa08f6036e153ce01a0412784ef6640355bfc2bf
Ahmad-Omar <aw.omar7@gmail.com>
2021-08-10 17:53:44 +0300

    serve public Relates #14

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 require('env2')('.env');
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.set('port', process.env.PORT || 5000);
 
 module.exports = app;
